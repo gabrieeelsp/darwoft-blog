9ca2b851d810d4b5c74031305cc166dd6f682e5c
gabrieeelsp <gabrieeelsp@gmail.com>
2024-04-14 07:05:18 -0300

    Se agrego refresh del token cuando se solicita

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -6,6 +6,7 @@ const registerHandler = require('../handlers/auth/registerHandler');
 const loginHandler = require('../handlers/auth/loginHandler');
 const logoutHandler = require('../handlers/auth/logoutHandler');
 const meHandler = require('../handlers/auth/meHandler');
+const refreshHandler = require('../handlers/auth/refreshHandler');
 
 const router = Router();
 
@@ -13,5 +14,6 @@ router.post('/signup', validateRegister, registerHandler);
 router.post('/signin', validateLogin, loginHandler);
 router.post('/signout', authMiddleware, logoutHandler);
 router.get('/me', authMiddleware, meHandler);
+router.post('/refresh', refreshHandler);
 
 module.exports = router;
