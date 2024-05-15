89ef27a6e5d9c75fab786baa9826e60d7f314ab5
gabrieeelsp <gabrieeelsp@gmail.com>
2024-05-15 18:23:08 -0300

    Se agrego vista error 404

--- client/src/views/publications/Category.jsx
+++ client/src/views/publications/Category.jsx
@@ -1,20 +1,22 @@
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { getCategoryBySlug } from "../../features/app/appSlice"
-import { LastPosts } from "../../components"
+import { Error, LastPosts } from "../../components"
 import { capitalize } from "../../utils"
 
 const Category = () => {
     const { categorySlug } = useParams()    
     const category = useSelector((state) => getCategoryBySlug(state, categorySlug))
     
+    if (!category) return <Error />
     return (
         <>
             { 
-            <div>
+            <div className="">
                 <h1 className="ml-3 mt-2 text-2xl font-bold">{category && capitalize(category.name)}</h1>
                 <h2 className="ml-3  text-sm font-bold">{category && capitalize(category.description)}</h2>
                 {category && <LastPosts categoryId={category._id} />}
+                
             </div>
             }
         </>
